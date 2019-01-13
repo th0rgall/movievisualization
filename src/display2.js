423f7e48aaa0badc3f3521fcd54ee3f5e79a1d9e
Thor Galle <thor.galle@student.kuleuven.be>
2019-01-13 14:55:00 +0200

    Include images

--- src/display2.js
+++ src/display2.js
@@ -1,18 +1,49 @@
 import * as d3 from 'd3';
 
 export default function display2(data) {
+
+    let toIndividualData = (weeklydata) => {
+        return weeklydata.reduce((acc, c, i, arr) => {
+
+            let mapfilm =  (f, fi, fa) => {
+                f.weekCount = c.weekCount;
+                f.week = c.week;
+                f.filmCount = fa.indexOf(f);
+                return f;
+            };
+
+            if (c.films && c.films.length > 0) {
+                return [].concat(acc, c.films.map(mapfilm));
+            } else {
+                return acc;
+            }
+        }, []);
+    };
+
+    let individualData = toIndividualData(data);
     
     var margin = {top: 20, right: 20, bottom: 70, left: 40},
-    width = 2000 - margin.left - margin.right,
-    height = 200 - margin.top - margin.bottom;
+    width = 6000 - margin.left - margin.right;
+
+    // var x = d3.scaleOrdinal().rangeRoundBands([0, width], .05);
+    var x = d3.scaleBand().domain(data.map(function(d) { return d.week; }))
+    .range([0, width]).paddingInner(0.15);
+
+    let posterRatio = 1.48;
+    let maxFilmCount = d3.max(individualData.map(f => f.filmCount));
+    let height = (x.bandwidth()) * posterRatio * maxFilmCount + margin.top + margin.bottom;
+
+   // height = 400 - margin.top - margin.bottom;
+
+    // poster ratio 1.48
 
     // Parse the date / time
     var	parseDate = d3.timeFormat("%Y-%m").parse;
 
-    // var x = d3.scaleOrdinal().rangeRoundBands([0, width], .05);
-    var x = d3.scaleBand().range([0, width]);
 
-    var y = d3.scaleLinear().range([height, 0]);
+    //var y = d3.scaleLinear().rangeRound([height, 0]);
+    let enumToMax = m => {let a = []; for (let i = 1; i <= m; i++) {a[i-1] = i} return a};
+    var y = d3.scaleBand().rangeRound([height, 0]).paddingInner(0.1);
 
     var xAxis = d3.axisBottom(x)
     .tickFormat(d3.timeFormat("%Y-%m"));
@@ -33,8 +64,8 @@ export default function display2(data) {
     //     d.value = +d.value;
     // });
 
-    x.domain(data.map(function(d) { return d.week; }));
-    y.domain([0, d3.max(data, function(d) { return d.weekCount; })]);
+    //y.domain([0, d3.max(data, function(d) { return d.weekCount; })]);
+    y.domain(enumToMax(maxFilmCount))
 
     let mouseG = svg.append("g");
     mouseG
@@ -78,27 +109,33 @@ export default function display2(data) {
     .attr("x", 9)
     .attr("dy", ".35em");
 
-    svg.selectAll("bar")
-    .data(data)
-    .enter().append("rect")
-    .style("fill", "steelblue")
+    let bars = svg.selectAll("bar")
+    //.data(data)
+    .data(individualData.filter(d => d.filmCount ? true : null))
+    .enter();
+    
+    bars.append(d => d.imgUrl ? document.createElementNS('http://www.w3.org/2000/svg', "image") 
+        : document.createElementNS('http://www.w3.org/2000/svg', "rect"))
+    //.style("fill", "steelblue")
     .attr("x", function(d) { return x(d.week); })
     .attr("width", x.bandwidth())
-    .attr("y", function(d) { return y(d.weekCount); })
-    .attr("height", function(d) { return height - y(d.weekCount); })
+    .attr("y", function(d) { return y(d.filmCount); })
+    .attr("height", y.bandwidth())
+    .attr("xlink:href", function(d) { return d.imgUrl ? d.imgUrl : null})
+    .attr("fill", function(d) {return d.imgUrl ? null : "grey"})
     .on('mouseover', function(d) {
         // focus.attr("transform", "translate(" + x(formatDate.parse(tar_date)) + ","+y(tar_value)+ ")");
         //focus.attr("transform", "translate(" + x(d.week) + "," + height - y(d.weekCount)+ ")");
-        tooltip.html(`${d3.timeFormat("%Y-%b-%e")(d.week)}\n${d.films.map(f => f.title).join(', ')}`)
+        tooltip.html(`${d3.timeFormat("%Y-%b-%e")(d.week)}\n${d.title}`)
         .style("visibility", "visible")
         // .style("top", d3.mouse(this)[1] - (tooltip[0][0].clientHeight - 30) + "px")
         // .style("left", d3.mouse(this)[0] - (tooltip[0][0].clientWidth / 2.0) + "px");
         .style("top", d3.mouse(document.querySelector('body'))[1] + "px")
         .style("left", d3.mouse(document.querySelector('body'))[0] + "px");
 
-        d3.select(this).style("fill", "rgb(255,255,0)");
+        //d3.select(this).style("fill", "rgb(255,255,0)");
     })
     .on('mouseout', function(d) {
-        d3.select(this).style("fill", "steelblue");
+        //d3.select(this).style("fill", "steelblue");
     })
 }
